cf18c24cab972cc0cca962ae15ea0a743c3d4829
Artem Galkin <greatjudge0@gmail.com>
2021-05-24 14:33:47 +0400

    chage list order in dest

--- state.py
+++ state.py
@@ -101,7 +101,7 @@ class State:
                                                                       element['status'].lower() == 'ok'
                                                                       and addr
                                                                       and place.location]
-                all.sort(key=lambda x: x[0][1], reverse=True)
+                all.sort(key=lambda x: x[0][1])
                 return all[:count]
             else:
                 return []
